7afea3806f44bb78432a843bf27b6496402c15e5
esccobar <nikesccobar10@gmail.com>
2017-02-15 17:38:38 -0600

    Update PjtRepositorio1.java
    esta es una moficación desde la web

--- src/pjtrepositorio1/PjtRepositorio1.java
+++ src/pjtrepositorio1/PjtRepositorio1.java
@@ -20,5 +20,7 @@ public class PjtRepositorio1 {
         System.out.println("hola nik");
         System.out.println("hola niko");
         System.out.println("hola ni");
+        System.out.println("hola esta es una moficacion desde la web");
+        
     }
 }
