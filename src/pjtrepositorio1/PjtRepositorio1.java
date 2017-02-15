bcfbd440dc95e7762d7bc66f620aaa919cc37dfc
adminNIES <adminNIES@192.168.8.100>
2017-02-14 20:56:02 -0600

    mi aporte es el hola mundo en el archvio hola nico en el archivo pjtRepositorio.java

--- src/pjtrepositorio1/PjtRepositorio1.java
+++ src/pjtrepositorio1/PjtRepositorio1.java
@@ -17,6 +17,7 @@ public class PjtRepositorio1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("hola nico");
     }
     
 }
